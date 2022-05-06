807be21d58783149764aba15c790dca6c829ba77
Victor Turra <victor.florencio@univem.edu.br>
2022-05-06 11:23:13 -0300

    feat: Responde user creation with 201
    - Added route
    - Added users controller and method

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->post('/users', [
+    'as' => 'users.create',
+    'uses' => 'UserController@create'
+]);
