4fc428da7a2697804d96ced22856940c4168c25b
Victor Turra <victor.florencio@univem.edu.br>
2022-05-06 11:11:46 -0300

    test: User creation use case

---
+++ tests/CreateUserTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class CreateUserTest extends TestCase
+{
+    private $createUserRequestBody = [
+        'name' => 'John Doe',
+        'email' => '',
+        'password' => '',
+        'password_confirmation' => '',
+    ];
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testShouldCreateAnUser()
+    {
+        $this->post('/users', $this->createUserRequestBody);
+
+        $this->seeStatusCode(201);
+        $this->seeJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'email',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+}
