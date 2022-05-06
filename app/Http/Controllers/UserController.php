943e7853147cdf5c42a85db6eae69dd14be17fb7
Victor Turra <victor.florencio@univem.edu.br>
2022-05-06 12:10:52 -0300

    feat: Add password validation and confirmation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function create(Request $request){
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
         $user = $request->all();
 
         $user['id'] = rand(1, 100);
