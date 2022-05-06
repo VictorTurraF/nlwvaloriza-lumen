b5816229f5fe0199f293b4953284a9e52f9b90db
Victor Turra <victor.florencio@univem.edu.br>
2022-05-06 11:31:29 -0300

    feat: Respond user created with aditional fields

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,10 @@ class UserController extends Controller
     public function create(Request $request){
         $user = $request->all();
 
+        $user['id'] = rand(1, 100);
+        $user['created_at'] = date('Y-m-d H:i:s');
+        $user['updated_at'] = date('Y-m-d H:i:s');
+
         return response()->json($user, 201);
     }
 }
